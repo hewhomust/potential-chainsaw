0a26742531735322368bbc924b8d2949d38c7d8c
dean-l <bean-44@hotmail.com>
2016-10-11 16:12:25 +1000

    Added XML loading script. Prelim test.

---
+++ Prototype3/getArticles.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	var articleDoc = new XMLHttpRequest();
+	articleDoc.onreadystatechange = function(){
+		if(articleDoc.readyState == 4 && articleDoc.status == 200){
+			loadArticleXML(articleDoc);
+		}
+	};
+	articleDoc.open("GET", "articles.xml", true);
+	articleDoc.send();
+		
+	function loadArticlesXML(doc){
+		var xmlArticles = doc.responseXML;
+		var xmlArticle = xmlArticles.getElementsByTagName('article');
+		var title = xmlArticle[i].getElementsByTagName('title')[0].childNodes[0].nodeValue;
+		var article = xmlArticle[i].getElementsByTagName('text')[0].childNodes[0].nodeValue;
+		document.getElementById("mainArticleTitle").innerHTML = title;
+		document.getElementById("mainArticleText").innerHTML = article;
+	}
+});
